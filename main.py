261283c6f11c4d72af28c311ac54923a4319dffa
ju020813-source <ju020813@gmail.com>
2025-11-10 12:36:03 +0900

    Add geometry calculations to main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from geo import circle_area, circumference, rect_area, tri_area
+
+if __name__ == "__main__":
+    print("circle area:", circle_area(5))
+    print("circle circumference:", circumference(5))
+    print("rectangle area:", rect_area(4, 6))
+    print("triangle area:", tri_area(3, 8))
